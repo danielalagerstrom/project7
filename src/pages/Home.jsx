aab3c2fd9f87ff060d7a5228ba33eeca0025b50f
Daniela Lagerström <eaa24dcl@students.eaaa.dk>
2025-02-15 15:47:57 +0100

    version12

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
-import Hero from "../components/HeroSection";
 import HeroSection from "../components/HeroSection";
 
 const Home = () => {
